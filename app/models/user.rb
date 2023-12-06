933668aa41b7dca45183cfbfa62f6d31c2925ebd
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-12-06 05:56:33 +0000

    added user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many  :goals, dependent: :destroy
+  has_many  :workouts, dependent: :destroy
 end
