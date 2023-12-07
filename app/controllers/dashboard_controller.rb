c0f156344f6f259d45ee1881f88934a03d88f7c5
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-12-07 21:13:01 +0000

    added current goal

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,10 @@ class DashboardController < ApplicationController
 
     @list_of_goals = matching_goals.order({ :created_at => :desc })
 
+    #most recent goal
+    @most_recent_goal = @list_of_goals.first.goal
+
     render({ :template => "dashboard/index" })
   end
+
 end
