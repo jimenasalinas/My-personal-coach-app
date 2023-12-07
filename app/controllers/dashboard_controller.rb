03daff61db2149a48de9bff218bcfb9e47dbc136
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-12-07 21:35:05 +0000

    added days till goal

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,9 +5,22 @@ class DashboardController < ApplicationController
     @list_of_goals = matching_goals.order({ :created_at => :desc })
 
     #most recent goal
-    @most_recent_goal = @list_of_goals.first.goal
+    @most_recent_goal = @list_of_goals.first&.goal
+    @most_recent_due_date = @list_of_goals.first&.goal_due_date
+
+    @days_till_goal = days_until_goal(@most_recent_due_date)
+
 
     render({ :template => "dashboard/index" })
   end
 
+  def days_until_goal(goal_due_date)
+    return nil unless goal_due_date
+
+    today = Date.today
+    due_date = goal_due_date.to_date
+
+    # Calculate the difference in days
+    (due_date - today).to_i
+  end
 end
--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -5,7 +5,7 @@ class GoalsController < ApplicationController
     @list_of_goals = matching_goals.order({ :created_at => :desc })
 
     #most recent goal
-    @most_recent_goal = @list_of_goals.first.goal
+    @most_recent_goal = @list_of_goals.first&.goal
 
     render({ :template => "goals/index" })
   end
