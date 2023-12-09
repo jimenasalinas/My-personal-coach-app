89e2426f0dfc556c369e0e1b056b0aa2ea7b0890
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-12-09 01:38:35 +0000

    personalized content for each user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    matching_goals = Goal.all
+    matching_goals = Goal.where({ :user_id => current_user.id })
 
     @list_of_goals = matching_goals.order({ :created_at => :desc })
 
